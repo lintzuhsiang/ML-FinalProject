a88ea94b8fc33faeb7a2ea62fdfc4549fbb2b509
Herman <Herman>
2016-12-14 16:40:58 +0800

    Allow to combine features for train/test data.
    the usage of combine_feature.py has changed
    1) need to specify the data set (train/test/both(default))
    2) change the second argument to working directory(./workspace/)

--- src/combine_features.py
+++ src/combine_features.py
@@ -1,6 +1,7 @@
 # For NTU 2016 Fall Machine Learning Class
 # Final Project: OutBrain Click Prediction
-# Usage: combine_features.py ../data/ ../workspace/train_combined_features.csv
+# Usage: combine_features.py ../data/ ../workspace/ --mode both
+# Note: pass [--mode train] or [--mode test] to do on train/test data only, default is both
 
 import argparse
 import numpy as np
@@ -10,124 +11,140 @@ import pandas as pd
 """ parser """
 parser = argparse.ArgumentParser()
 parser.add_argument( "dataDirectory", help='directory that contains training/test data' )
-parser.add_argument( "outputFile", help='path of the output file' )
+parser.add_argument( "workingDirectory", help='directory that contains working data' )
+parser.add_argument( "--mode", help='execute on train data, test data or both', default='both', \
+												choices=['both', 'train', 'test'] )
 parser.add_argument( "--printInfo", help='true to print info', default=False )
 args = parser.parse_args()
 
+""" determine dataset (train or test or both) """
+if( args.mode=='both' )
+	DataSet = ['train', 'test']
+elif( args.mode=='train' )
+	DataSet = ['train']
+else # args.mode=='test'
+	DataSet = ['test']
 
-""" read train data """
-if( args.printInfo ):
-	print "Extract_features.py: reading input data - clicks_train.csv"
-train_data = pd.read_csv( args.dataDirectory + 'clicks_train.csv', delimiter=',', \
+for data_set in DataSet:
+	""" read train data """
+	if( data_set=='train' )	
+		if( args.printInfo ):
+			print "Extract_features.py: reading input data - clicks_train.csv"
+		data = pd.read_csv( args.dataDirectory + 'clicks_train.csv', delimiter=',', \
 													dtype={'display_id':np.int32, 'ad_id':np.int32, 'clicked':bool})
 
+	if( data_set=='test' )
+		if( args.printInfo ):
+			print "Extract_features.py: reading input data - clicks_test.csv"
+		data = pd.read_csv( args.dataDirectory + 'clicks_test.csv', delimiter=',', \
+													dtype={'display_id':np.int32, 'ad_id':np.int32})													
 
-""" concatenate events features into train_data """
-# read events data
-if( args.printInfo ):
-	print "Extract_features.py: reading input data - events.csv"
-#events_data = np.genfromtxt( args.dataDirectory + 'events.csv', dtype='int64', delimiter=',', \
-#															skip_header=1, usecols =[0,1,2,3,4], converters = {1: lambda s: int(s, 16)})
-events_data = pd.read_csv( args.dataDirectory + 'events.csv', delimiter=',', usecols =[0,1,2,3,4], na_values='\N', \
-														converters = {1: lambda s: int(s, 16)}, dtype={'display_id':np.int32, \
-														'uuid':np.int64, 'document_id':np.int32, 'timestamp':np.int32, 'platform':str})
-								
-if( args.printInfo ):
-	print "Extract_features.py: processing input data - events.csv"					
-events_data.fillna(-1, inplace=True)
-events_data['platform'] = events_data['platform'].astype('int8')
+	""" concatenate events features into train/test data """
+	# read events data
+	if( args.printInfo ):
+		print "Extract_features.py: reading input data - events.csv"
+	#events_data = np.genfromtxt( args.dataDirectory + 'events.csv', dtype='int64', delimiter=',', \
+	#															skip_header=1, usecols =[0,1,2,3,4], converters = {1: lambda s: int(s, 16)})
+	events_data = pd.read_csv( args.dataDirectory + 'events.csv', delimiter=',', usecols =[0,1,2,3,4], na_values='\N', \
+															converters = {1: lambda s: int(s, 16)}, dtype={'display_id':np.int32, \
+															'uuid':np.int64, 'document_id':np.int32, 'timestamp':np.int32, 'platform':str})
+									
+	if( args.printInfo ):
+		print "Extract_features.py: processing input data - events.csv"					
+	events_data.fillna(-1, inplace=True)
+	events_data['platform'] = events_data['platform'].astype('int8')
 
-# search for the corresponding rows in events_data by display_id
-in_array = np.in1d( train_data[:]['display_id'], events_data[:]['display_id'] )
-idx_found = np.searchsorted( events_data[:]['display_id'], train_data[:]['display_id'] )
-idx_in_limit = ( idx_found != len(events_data) )
-idx_found = idx_found * np.logical_and(idx_in_limit, in_array) + (-1)*( np.logical_not(np.logical_and(idx_in_limit, in_array)) )
-idx_found_bool = np.concatenate(( ( idx_found > -1 ).reshape(-1,1), ( idx_found > -1 ).reshape(-1,1)), axis=1)
-idx_found_bool = np.concatenate(( idx_found_bool, idx_found_bool), axis=1)
-to_append = events_data.iloc[idx_found, 1:5] * (idx_found_bool) + np.array([-1,-1,-1,-1])*(1-(idx_found_bool))
-to_append.reset_index( drop=True, inplace=True )
-to_append.rename(columns={'document_id': 'display_document_id'}, inplace=True)
+	# search for the corresponding rows in events_data by display_id
+	in_array = np.in1d( data[:]['display_id'], events_data[:]['display_id'] )
+	idx_found = np.searchsorted( events_data[:]['display_id'], data[:]['display_id'] )
+	idx_in_limit = ( idx_found != len(events_data) )
+	idx_found = idx_found * np.logical_and(idx_in_limit, in_array) + (-1)*( np.logical_not(np.logical_and(idx_in_limit, in_array)) )
+	idx_found_bool = np.concatenate(( ( idx_found > -1 ).reshape(-1,1), ( idx_found > -1 ).reshape(-1,1)), axis=1)
+	idx_found_bool = np.concatenate(( idx_found_bool, idx_found_bool), axis=1)
+	to_append = events_data.iloc[idx_found, 1:5] * (idx_found_bool) + np.array([-1,-1,-1,-1])*(1-(idx_found_bool))
+	to_append.reset_index( drop=True, inplace=True )
+	to_append.rename(columns={'document_id': 'display_document_id'}, inplace=True)
 
-# concatenate the corresponding rows into train data
-train_data = pd.concat( (train_data, to_append), axis=1 )
-
-
-""" concatenate documents_meta features into train_data (display documents) """
-# read documents_meta data
-if( args.printInfo ):
-	print "Extract_features.py: reading input data - documents_meta.csv"	
-# documents_meta_data = np.genfromtxt( args.dataDirectory + 'documents_meta.csv', dtype='int64', skip_header=1, delimiter=',')
-documents_meta_data = pd.read_csv( args.dataDirectory + 'documents_meta.csv', delimiter=',', usecols =[0,1,2], \
-																		dtype={'document_id':np.int32})																	
-documents_meta_data.fillna(-1, inplace=True)
-documents_meta_data['source_id'] = documents_meta_data['source_id'].astype('int32')
-documents_meta_data['publisher_id'] = documents_meta_data['publisher_id'].astype('int32')
-
-if( args.printInfo ):
-	print "Extract_features.py: processing input data - documents_meta.csv (for display documents)"	
-# search for the corresponding row in events_data by display_id
-documents_meta_data.sort_values( ['document_id'], inplace=True )
-in_array = np.in1d( train_data[:]['display_document_id'], documents_meta_data[:]['document_id'] )
-idx_found = np.searchsorted( documents_meta_data[:]['document_id'], train_data[:]['display_document_id'] )
-idx_in_limit = ( idx_found != len(documents_meta_data) )
-idx_found = idx_found * np.logical_and(idx_in_limit, in_array) + (-1)*( np.logical_not(np.logical_and(idx_in_limit, in_array)) )
-idx_found_bool = np.concatenate(( ( idx_found > -1 ).reshape(-1,1), ( idx_found > -1 ).reshape(-1,1) ), axis=1)
-to_append = documents_meta_data.iloc[idx_found, 1:3] * (idx_found_bool) + np.array([-1,-1])*(1-(idx_found_bool))
-to_append.reset_index( drop=True, inplace=True )
-to_append.rename(columns={'source_id': 'display_source_id', 'publisher_id': 'display_publisher_id'}, inplace=True)
-
-# concatenate the corresponding rows into train data
-train_data = pd.concat( (train_data, to_append), axis=1 )
+	# concatenate the corresponding rows into train data
+	data = pd.concat( (data, to_append), axis=1 )
 
 
-""" concatenate promoted_content features into train_data """
-if( args.printInfo ):
-	print "Extract_features.py: reading promoted_content.csv"															
-# promoted_content_data = np.genfromtxt( args.dataDirectory + 'promoted_content.csv', dtype='int64', skip_header=1, delimiter=',')
-promoted_content_data = pd.read_csv( args.dataDirectory + 'promoted_content.csv', delimiter=',', \
-																			dtype={'ad_id':np.int32, 'document_id':np.int32,  \
-																			'campaign_id':np.int32, 'advertiser_id':np.int32})
+	""" concatenate documents_meta features into train/test data (display documents) """
+	# read documents_meta data
+	if( args.printInfo ):
+		print "Extract_features.py: reading input data - documents_meta.csv"	
+	# documents_meta_data = np.genfromtxt( args.dataDirectory + 'documents_meta.csv', dtype='int64', skip_header=1, delimiter=',')
+	documents_meta_data = pd.read_csv( args.dataDirectory + 'documents_meta.csv', delimiter=',', usecols =[0,1,2], \
+																			dtype={'document_id':np.int32})																	
+	documents_meta_data.fillna(-1, inplace=True)
+	documents_meta_data['source_id'] = documents_meta_data['source_id'].astype('int32')
+	documents_meta_data['publisher_id'] = documents_meta_data['publisher_id'].astype('int32')
 
-# search for the corresponding rows in promoted_content_data by display_id
-in_array = np.in1d( train_data[:]['ad_id'], promoted_content_data[:]['ad_id'] )
-idx_found = np.searchsorted( promoted_content_data[:]['ad_id'], train_data[:]['ad_id'] )
-idx_in_limit = ( idx_found != len(promoted_content_data) )
-idx_found = idx_found * np.logical_and(idx_in_limit, in_array) + (-1)*( np.logical_not(np.logical_and(idx_in_limit, in_array)) )
-idx_found_bool = np.concatenate(( ( idx_found > -1 ).reshape(-1,1), ( idx_found > -1 ).reshape(-1,1)), axis=1)
-idx_found_bool = np.concatenate(( idx_found_bool, ( idx_found > -1 ).reshape(-1,1) ), axis=1)
-to_append = promoted_content_data.iloc[idx_found, 1:4] * (idx_found_bool) + np.array([-1,-1,-1])*(1-(idx_found_bool))
-to_append.reset_index( drop=True, inplace=True )
-to_append.rename(columns={'document_id': 'ad_document_id'}, inplace=True)
+	if( args.printInfo ):
+		print "Extract_features.py: processing input data - documents_meta.csv (for display documents)"	
+	# search for the corresponding row in events_data by display_id
+	documents_meta_data.sort_values( ['document_id'], inplace=True )
+	in_array = np.in1d( data[:]['display_document_id'], documents_meta_data[:]['document_id'] )
+	idx_found = np.searchsorted( documents_meta_data[:]['document_id'], data[:]['display_document_id'] )
+	idx_in_limit = ( idx_found != len(documents_meta_data) )
+	idx_found = idx_found * np.logical_and(idx_in_limit, in_array) + (-1)*( np.logical_not(np.logical_and(idx_in_limit, in_array)) )
+	idx_found_bool = np.concatenate(( ( idx_found > -1 ).reshape(-1,1), ( idx_found > -1 ).reshape(-1,1) ), axis=1)
+	to_append = documents_meta_data.iloc[idx_found, 1:3] * (idx_found_bool) + np.array([-1,-1])*(1-(idx_found_bool))
+	to_append.reset_index( drop=True, inplace=True )
+	to_append.rename(columns={'source_id': 'display_source_id', 'publisher_id': 'display_publisher_id'}, inplace=True)
 
-# concatenate the corresponding rows into train data
-train_data = pd.concat( (train_data, to_append), axis=1 )
-
-
-""" concatenate documents_meta features into train_data (ad documents) """
-if( args.printInfo ):
-	print "Extract_features.py: processing input data - documents_meta.csv (for ad documents)"	
-
-# search for the corresponding row in events_data by display_id
-# documents_meta_data.sort_values( ['document_id'], inplace=True )
-in_array = np.in1d( train_data[:]['ad_document_id'], documents_meta_data[:]['document_id'] )
-idx_found = np.searchsorted( documents_meta_data[:]['document_id'], train_data[:]['ad_document_id'] )
-idx_in_limit = ( idx_found != len(documents_meta_data) )
-idx_found = idx_found * np.logical_and(idx_in_limit, in_array) + (-1)*( np.logical_not(np.logical_and(idx_in_limit, in_array)) )
-idx_found_bool = np.concatenate(( ( idx_found > -1 ).reshape(-1,1), ( idx_found > -1 ).reshape(-1,1) ), axis=1)
-to_append = documents_meta_data.iloc[idx_found, 1:3] * (idx_found_bool) + np.array([-1,-1])*(1-(idx_found_bool))
-to_append.reset_index( drop=True, inplace=True )
-to_append.rename(columns={'source_id': 'ad_source_id', 'publisher_id': 'ad_publisher_id'}, inplace=True)
-
-# concatenate the corresponding rows into train data
-train_data = pd.concat( (train_data, to_append), axis=1 )
+	# concatenate the corresponding rows into train data
+	data = pd.concat( (data, to_append), axis=1 )
 
 
-""" write data to a csv file """
-if( args.printInfo ):
-	print "Extract_features.py: writing output to a csv file"
-# column_names = "display_id, uuid, document_id, timestamp, platform, campaign_id, advertiser_id, source_id, publisher_id, publish_time"
-# np.savetxt( args.outputFile, output_to_file, delimiter=',', header=column_names, comments='', fmt='%i' )
-train_data.to_csv( args.outputFile, sep=',' )
+	""" concatenate promoted_content features into train/test data """
+	if( args.printInfo ):
+		print "Extract_features.py: reading promoted_content.csv"															
+	# promoted_content_data = np.genfromtxt( args.dataDirectory + 'promoted_content.csv', dtype='int64', skip_header=1, delimiter=',')
+	promoted_content_data = pd.read_csv( args.dataDirectory + 'promoted_content.csv', delimiter=',', \
+																				dtype={'ad_id':np.int32, 'document_id':np.int32,  \
+																				'campaign_id':np.int32, 'advertiser_id':np.int32})
 
-train_data_reduced = train_data.iloc[:, [3,6,7,8,9,11,12,13,14]]
-train_data_reduced.to_csv( 'reduced_' + args.outputFile+, sep=',' )
+	# search for the corresponding rows in promoted_content_data by display_id
+	in_array = np.in1d( data[:]['ad_id'], promoted_content_data[:]['ad_id'] )
+	idx_found = np.searchsorted( promoted_content_data[:]['ad_id'], data[:]['ad_id'] )
+	idx_in_limit = ( idx_found != len(promoted_content_data) )
+	idx_found = idx_found * np.logical_and(idx_in_limit, in_array) + (-1)*( np.logical_not(np.logical_and(idx_in_limit, in_array)) )
+	idx_found_bool = np.concatenate(( ( idx_found > -1 ).reshape(-1,1), ( idx_found > -1 ).reshape(-1,1)), axis=1)
+	idx_found_bool = np.concatenate(( idx_found_bool, ( idx_found > -1 ).reshape(-1,1) ), axis=1)
+	to_append = promoted_content_data.iloc[idx_found, 1:4] * (idx_found_bool) + np.array([-1,-1,-1])*(1-(idx_found_bool))
+	to_append.reset_index( drop=True, inplace=True )
+	to_append.rename(columns={'document_id': 'ad_document_id'}, inplace=True)
+
+	# concatenate the corresponding rows into train/test data
+	data = pd.concat( (data, to_append), axis=1 )
+
+
+	""" concatenate documents_meta features into train/test data (ad documents) """
+	if( args.printInfo ):
+		print "Extract_features.py: processing input data - documents_meta.csv (for ad documents)"	
+
+	# search for the corresponding row in events_data by display_id
+	# documents_meta_data.sort_values( ['document_id'], inplace=True )
+	in_array = np.in1d( data[:]['ad_document_id'], documents_meta_data[:]['document_id'] )
+	idx_found = np.searchsorted( documents_meta_data[:]['document_id'], data[:]['ad_document_id'] )
+	idx_in_limit = ( idx_found != len(documents_meta_data) )
+	idx_found = idx_found * np.logical_and(idx_in_limit, in_array) + (-1)*( np.logical_not(np.logical_and(idx_in_limit, in_array)) )
+	idx_found_bool = np.concatenate(( ( idx_found > -1 ).reshape(-1,1), ( idx_found > -1 ).reshape(-1,1) ), axis=1)
+	to_append = documents_meta_data.iloc[idx_found, 1:3] * (idx_found_bool) + np.array([-1,-1])*(1-(idx_found_bool))
+	to_append.reset_index( drop=True, inplace=True )
+	to_append.rename(columns={'source_id': 'ad_source_id', 'publisher_id': 'ad_publisher_id'}, inplace=True)
+
+	# concatenate the corresponding rows into train data
+	data = pd.concat( (data, to_append), axis=1 )
+
+
+	""" write data to a csv file """
+	if( args.printInfo ):
+		print "Extract_features.py: writing output to a csv file"
+	# column_names = "display_id, uuid, document_id, timestamp, platform, campaign_id, advertiser_id, source_id, publisher_id, publish_time"
+	# np.savetxt( args.outputFile, output_to_file, delimiter=',', header=column_names, comments='', fmt='%i' )
+	data.to_csv( args.workingDirectory + data_set + '_combined_features_reduced.csv', sep=',' )
+
+	data_reduced = data.iloc[:, [3,6,7,8,9,11,12,13,14]]
+	data_reduced.to_csv( args.workingDirectory + data_set + '_combined_features_reduced.csv', sep=',' )
